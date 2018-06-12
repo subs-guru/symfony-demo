96adb93ff0ef4a72e4feeceefc0d33f4f0843ecf
Fabrice COTTIN <fabrice.cottin1@viacesi.fr>
2018-06-12 11:29:07 +0200

    route, controller et views

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomePageController extends Controller
+{
+    public function homepageAction()
+    {
+        die('ok');
+        //return $this->render('views/homepage.twig');
+    }
+}
